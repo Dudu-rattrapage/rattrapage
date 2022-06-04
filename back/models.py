0b4a0d5d3f015e35d60fa46f616e3040ebed8f1a
Dudu4064 <ducasseque@eisti.eu>
2022-06-04 18:39:42 +0200

    add json entities

--- back/models.py
+++
@@ -1,6 +0,0 @@
-from mongoengine import Document, StringField, IntField
-
-class Countries(Document):
-  name = StringField(max_length=100)
-  capitale = StringField(max_length=100)
-  id = IntField()
