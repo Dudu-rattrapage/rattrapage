1e0f4bcbee4cf5c6da4ee417967192787a3cba3c
Dudu4064 <ducasseque@eisti.eu>
2022-06-04 21:13:59 +0200

    add insert and get function

--- back/main.py
+++ back/main.py
@@ -3,6 +3,8 @@
 from fastapi import FastAPI,HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+
 app = FastAPI()
 
 origins = ["*"]
@@ -14,11 +16,33 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def read_item():
-  return {"Hello World"}
-
+    return {"Hello World"}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
   return {"item_id": item_id}
+
+
+def insertValues(TimeStamp,Ip,Domain,Description,Ping,PacketLoss):
+    conn = sqlite3.connect('BDD/rattrapage.db')
+    c = conn.cursor()
+    data = (TimeStamp, Ip, Domain, Description, Ping, PacketLoss)
+    c.execute("INSERT INTO ping_request_results (TimeStamp, IP, Domain, Description, Ping, PacketLoss) VALUES (?,?,?,?,?,?);"
+    ,data)
+    print("Values insert sucessfuly")
+    conn.commit()
+    conn.close()
+
+def getAll():
+    conn = sqlite3.connect('BDD/rattrapage.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM ping_request_results")
+    list = c.fetchall()
+    conn.commit()
+    conn.close()
+    print(list)
+    return list
+
